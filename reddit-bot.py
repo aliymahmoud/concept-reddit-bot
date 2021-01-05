db227975d62d5f2a22b12c62f9a4b45d83abf2e9
Ali Mahmoud <ali.mahmoud@at-portal.info>
2021-01-06 00:20:21 +0200

    setting up cardentials

--- reddit-bot.py
+++ reddit-bot.py
@@ -1,37 +1,45 @@
 import praw
 from urllib.parse import quote_plus
 
-triggers = ["what is", "who is", "what are"]
-reply = "[Let me google that for you](http://lmgtfy.com/?q={})"
-client_id = "nFQTrQaZIbEMDQ"
-client_secret = "RRbBcidEdLzr2yCkzFZ_zEiwJC6QKg"
-username = "java-tar"
-password = "M-1dest4000"
-user_agent = "personal LMGTFY beta (by u/java-tar)"
+
+client_id = ""
+client_secret = ""
+username = ""
+password = ""
+user_agent = ""
+
+
+QUESTIONS = ["what is", "who is", "what are"]
+REPLY_TEMPLATE = "[Let me google that for you](http://lmgtfy.com/?q={})"
 
 
 def main():
-    reddit = praw.Reddit(client_id=client_id,
-                         client_secret=client_secret,
-                         username=username,
-                         password=password,
-                         user_agent=user_agent)
-    target_sub = "faketaskfcih"
-    subreddit = reddit.subreddit(target_sub)
+    reddit = praw.Reddit(
+        user_agent=user_agent,
+        client_id=client_id,
+        client_secret=client_secret,
+        username=username,
+        password=password,
+    )
+
+    subreddit = reddit.subreddit("faketaskfcih")
     for submission in subreddit.stream.submissions():
         process_submission(submission)
 
 
 def process_submission(submission):
+    # Ignore titles with more than 10 words as they probably are not simple questions.
     if len(submission.title.split()) > 10:
         return
+
     normalized_title = submission.title.lower()
-    for question_phrase in triggers:
+    for question_phrase in QUESTIONS:
         if question_phrase in normalized_title:
             url_title = quote_plus(submission.title)
-            reply_text = reply.format(url_title)
+            reply_text = REPLY_TEMPLATE.format(url_title)
             print(f"Replying to: {submission.title}")
             submission.reply(reply_text)
+            # A reply has been made so do not attempt to match other phrases.
             break
 
 
